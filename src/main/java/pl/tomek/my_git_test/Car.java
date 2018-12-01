484f5b5784023f9a6373ece125dd397ad405fc51
tommu <tom.music@gazeta.pl>
2018-12-01 11:44:07 +0100

    [add] - rozszerzenie klasy Car

--- src/main/java/pl/tomek/my_git_test/Car.java
+++ src/main/java/pl/tomek/my_git_test/Car.java
@@ -6,5 +6,16 @@ public class Car {
     private String brand;
     private String model;
 
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
 
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
 }
