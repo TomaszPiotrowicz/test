3792029eb658ee3bdfe4fa6c8e1ad6bcc16d718c
tommu <tom.music@gazeta.pl>
2018-12-01 15:08:24 +0100

    [add] zmiany mokey

--- src/main/java/pl/tomek/my_git_test/Mokey.java
+++ src/main/java/pl/tomek/my_git_test/Mokey.java
@@ -1,10 +1,13 @@
 package pl.tomek.my_git_test;
 
+import javax.naming.Name;
+
 public class Mokey {
 
     @Override
     public String toString() {
-        return "Mokey{}";
+        return "hello{}";
+
     }
 
     public Mokey(String name){
