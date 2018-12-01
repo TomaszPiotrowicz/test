4d5866cd9dbfdedc914e9e535954d6be5686c17a
tommu <tom.music@gazeta.pl>
2018-12-01 13:08:27 +0100

    [change] class Animal addining getter setter

--- src/main/java/pl/tomek/my_git_test/Animal.java
+++ src/main/java/pl/tomek/my_git_test/Animal.java
@@ -3,6 +3,10 @@ package pl.tomek.my_git_test;
 public class Animal {
     String name;
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
